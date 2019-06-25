e9103013e571d765d299227d06a4b4869588f213
Guilherme Pressutto <gpressutto5@gmail.com>
2019-06-25 15:59:36 -0300

    Created cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,40 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
+    suits = ["♠︎", "♣︎", "♥︎", "♦︎"]
 
-  @doc """
-  Hello world.
+    for suit <- suits, value <- values do
+      "#{value}#{suit}"
+    end
+  end
 
-  ## Examples
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-      iex> Cards.hello()
-      :world
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
 
-  """
-  def hello do
-    :world
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "Something went wrong."
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
